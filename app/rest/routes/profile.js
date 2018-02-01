cf73ecd50cee89c22b5139b311d503ec7c9e3244
baduga <baduga@localhost>
2018-02-01 03:11:45 -0200

    postman and self profile

--- app/rest/routes/profile.js
+++ app/rest/routes/profile.js
@@ -3,13 +3,34 @@ var User = require('app/db/model/user');
 
 module.exports = function(server) {
 	
-    server.get('/user/profile/:uuid', function(req, res, next) {
+    server.get('/user/profile', function(req, res, next) {
+    	console.log('payload: ' + JSON.stringify(req.payload));
+    	var privatemask = {'_id' : 0, 'password': 0, 'token': 0,'__v' : 0};
+    	var mask = privatemask;
+    	User.findOne({"uuid" : req.payload.uuid}, mask).then(function(doc) {
+			if(doc===null) {
+				var statusCode = 404;
+				var message = {message: 'resource not found'};
+				res.send(statusCode, message);
+			} else {
+				res.send(200, doc);
+			}
+		}).catch(function(error){
+			var statusCode = 500;
+			var message = {message: 'error looking for user'};
+			console.log('error looking for user - [' + error + ']');
+			res.send(statusCode, message);
+		});
+        next();
+    });
+
+	server.get('/user/profile/:uuid', function(req, res, next) {
     	console.log('payload: ' + JSON.stringify(req.payload));
     	var publicmask = {'_id' : 0, 'uuid' : 1, 'name' : 1};
     	var privatemask = {'_id' : 0, 'password': 0, 'token': 0,'__v' : 0};
-    	var mask = publicmask;
-    	if(req.params.uuid===req.payload.uuid) {
-    		mask = privatemask;
+    	var mask = privatemask;
+    	if(req.params.uuid!==req.payload.uuid) {
+    		mask = publicmask;
     	}
     	User.findOne({"uuid" : req.params.uuid}, mask).then(function(doc) {
 			if(doc===null) {
